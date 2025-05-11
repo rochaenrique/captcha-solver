4fd0f0cfd1532c2bd15bc8e4b1e8d35d5e10797e
Enrique Rocha Benatti <rochabenattienrique@gmail.com>
2025-05-11 15:44:45 +0200

    made goat script

--- build.sh
+++ build.sh
@@ -13,15 +13,52 @@ echoval() {
 	eval $*
 }
 
-if [[ $1 = "download" ]]; then
+find_python() {
+	# try python3
+	if [[ -n $(which python3.9) ]]; then
+		echo "python3.9"
+	elif [[ -n $(which python3) ]]; then
+	   PY_VER=$(python3 --version | cut -d' ' -f2)
+	   if [[ $PY_VER =~ ^3\.9\.[0-9]{1,2}$ ]]; then
+		   echo "python3"
+	   fi
+	fi
+}
+
+validate_python() {
+	PYTHON=$(find_python)
+	if [[ -z $PYTHON ]]; then
+		echo could not find python3.9!
+		exit
+	fi
+}
+
+if [[ $1 = "env" ]]; then
+	if [[ ! -d .venv ]]; then
+		validate_python
+		echoval $PYTHON -m venv .venv
+	fi
+	if [[ -z $(which pip) ]]; then
+		echo could not find pip! activate env!
+		exit
+	fi
+	if [[ -e requirements.txt ]]; then
+		echoval pip install -r requirements.txt
+	else
+		echo no requirements.txt! get it!
+		exit
+	fi
+
+elif [[ $1 = "download" ]]; then
 	echo downloading!
 	if [[ ! -d data ]]; then
 		mkdir data
 	fi
-	echo kaggle competitions download -c yo-no-soy-un-bot -p data
-	cd data
-	unzip *
+	echoval kaggle competitions download -c yo-no-soy-un-bot -p data
+	echoval cd data
+	echoval unzip *
 else
+	validate_python
 	echo training!
 	SUBMIT="build/submit_$(date '+%d-%m_%H-%M-%S').csv"
 	echoval python3 model.py $SUBMIT
